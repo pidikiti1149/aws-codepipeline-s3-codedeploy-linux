0c07388d23fc5cc44719d0c3dcfa6aff706646d2
Kodeedudey <32563046+Kodeedudey@users.noreply.github.com>
2019-10-01 18:37:48 +0530

    Create tomcat.sh

---
+++ scripts/tomcat.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install default-jdk
+sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+cd /tmp
+wget https://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.26/bin/apache-tomcat-9.0.26.tar.gz
+sudo mkdir /opt/tomcat
+cd /opt/tomcat
+sudo tar xzvf /tmp/apache-tomcat-9.0.*tar.gz -C /opt/tomcat --strip-components=1
+sudo chgrp -R tomcat /opt/tomcat
+sudo chmod -R g+r conf
+sudo chmod g+x conf
+sudo chown -R tomcat webapps/ work/ temp/ logs/
+sudo cp tomcat.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo /opt/tomcat/bin/startup.sh run
+sudo ufw allow 8080
